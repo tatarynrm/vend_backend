830902d32ab9cf41a1a32c1d1c83306bf2407ccb
tatarynrm <tatarynrm@gmail.com>
2023-09-15 01:42:20 +0300

    dsa

--- controllers/smsStatus.js
+++ controllers/smsStatus.js
@@ -1,49 +1,67 @@
 const db = require("../db/db");
 
-const getAllTodaySms = async (req,res)=>{
-    try {
-        // const reuslt = await db.query(`select * from sms_status`)
-        const result = await db.query( `
+const getAllTodaySms = async (req, res) => {
+  try {
+    // const reuslt = await db.query(`select * from sms_status`)
+    const result = await db.query(`
         SELECT *
         FROM sms_status a
         FULL OUTER JOIN company b ON a.company_id = b.id
         FULL OUTER JOIN view_sms_price c ON a.sms_price = c.id
         WHERE DATE(a.created_at) >= current_date
         LIMIT 100
-      `)
-        console.log(result);
-        res.status(200).json(result.rows)
-    } catch (error) {
-        console.log(error);
-    }
-}
+      `);
+    console.log(result);
+    res.status(200).json(result.rows);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-const getSmsByDate = async (req,res) =>{
-    const {dateFrom,dateTo} = req.body;
-    try {
-if (dateFrom !== '' & dateTo !== '') {
-    const result = await db.query( `
+const getSmsByDate = async (req, res) => {
+  const { dateFrom, dateTo } = req.body;
+  try {
+    if ((dateFrom !== "") & (dateTo !== "")) {
+      const result = await db.query(`
     SELECT *
     FROM sms_status a
     FULL OUTER JOIN company b ON a.company_id = b.id
     FULL OUTER JOIN view_sms_price c ON a.sms_price = c.id
     WHERE DATE(a.created_at) >= '${dateFrom}' AND DATE(a.created_at) <= '${dateTo}'
-    LIMIT 100
-  `)
-  res.status(200).json(result.rows)
-}else {
-    res.status(400).json({
-        msg:"Bad Request!No Date"
-    })
-}
-    } catch (error) {
-        console.log(error);
-        res.status(400).json({
-            error
-        })
+  `);
+      res.status(200).json(result.rows);
+    } else {
+      res.status(400).json({
+        msg: "Bad Request!No Date",
+      });
     }
-}
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      error,
+    });
+  }
+};
+const getSmsByDateAndCompany = async (req, res) => {
+  const { dateFrom, dateTo,company_id } = req.body;
+  console.log(company_id);
+  try {
+    if ((dateFrom !== "") & (dateTo !== "")) {
+      const result = await db.query(`
+            SELECT *
+            FROM sms_status a
+            FULL OUTER JOIN company b ON a.company_id = b.id
+            FULL OUTER JOIN view_sms_price c ON a.sms_price = c.id
+            WHERE DATE(a.created_at) >= '${dateFrom}' AND DATE(a.created_at) <= '${dateTo}' and company_id = ${+company_id}
+          `);
+      res.status(200).json(result.rows);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
 module.exports = {
-    getAllTodaySms,
-    getSmsByDate
+  getAllTodaySms,
+  getSmsByDate,
+  getSmsByDateAndCompany,
 };
--- routes/smsStatus.js
+++ routes/smsStatus.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const {  getAllTodaySms, getSmsByDate } = require("../controllers/smsStatus");
+const {  getAllTodaySms, getSmsByDate, getSmsByDateAndCompany } = require("../controllers/smsStatus");
 
 
 const router = express.Router();
 router.route('/').get(getAllTodaySms)
 router.route('/by-date').post(getSmsByDate)
+router.route('/by-date-company').post(getSmsByDateAndCompany)
 
 module.exports = router;
