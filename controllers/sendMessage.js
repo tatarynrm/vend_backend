cff9f2ae5e4e16d152a3002b7f466a7b27b8066b
tatarynrm <tatarynrm@gmail.com>
2023-09-30 12:34:13 +0300

    dsada

--- controllers/sendMessage.js
+++ controllers/sendMessage.js
@@ -483,6 +483,177 @@ const changePin = async (req, res) => {
     });
   }
 };
+const changeNumber = async (req, res) => {
+  const { data } = req.body;
+console.log('DATA!!!!!!!!',data);
+  try {
+    const queryParams = {
+      grant_type: "password",
+      username: 380675576609,
+      password: "!0250910zXcDanil",
+    };
+    const headers = {
+      Authorization: "Basic aW50ZXJuYWw6aW50ZXJuYWw=",
+    };
+    const token = await axios.post(
+      "https://a2p.vodafone.ua/uaa/oauth/token",
+      null,
+      {
+        params: queryParams,
+        headers: headers,
+      }
+    );
+
+    if (token && data) {
+      const url =
+        "https://a2p.vodafone.ua/communication-event/api/communicationManagement/v2/communicationMessage/send";
+
+      const headers = {
+        "Content-Type": "application/json",
+        Accept: "*/*",
+        Authorization: `bearer ${token.data.access_token}`,
+      };
+      const data1 = {
+      
+        content: `pin=${+data.smsInfo.machine_pin};settelnum:1=${+data.newNumber};`,
+        type: "SMS",
+        receiver: [
+          { id: +data.smsInfo.id, phoneNumber: +data.smsInfo.machine_phone },
+        ],
+        sender: { id: "Vend Water" },
+        characteristic: [
+          { name: "DISTRIBUTION.ID", value: 5029972 },
+          { name: "VALIDITY.PERIOD", value: "000000000100000R" },
+        ],
+      };
+
+      const sendLitersData = await axios({
+        method: "post",
+        url: url,
+        headers: headers,
+        data: data1,
+      })
+        .then((response) => {
+          db.query(
+            `
+                     INSERT INTO sms_status (company_id,status,status_id,status_name)
+                     values (${+data.userData
+                       .company_id},'${"ACCEPTED"}',${7},'${"Заміна номера модуля"}')
+                     `,
+            (error, result) => {
+              if (error) {
+                console.error("Error executing query", error);
+              } else {
+                console.log("Query result:", result.rows);
+              }
+            }
+          );
+          res.status(200).json(response.data);
+          console.log(data);
+          db.query(
+            `update  water_machine set machine_phone = ${+data.smsInfo.machine_phone}
+             where machine_id =${+data.smsInfo.machine_id}
+            `
+          );
+
+          console.log("Response:", response.data);
+        })
+        .catch((error) => {
+          console.error("Error:", error.message);
+        });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      error: error,
+    });
+  }
+};
+const changeToken = async (req, res) => {
+  const { data } = req.body;
+
+  try {
+    const queryParams = {
+      grant_type: "password",
+      username: 380675576609,
+      password: "!0250910zXcDanil",
+    };
+    const headers = {
+      Authorization: "Basic aW50ZXJuYWw6aW50ZXJuYWw=",
+    };
+    const token = await axios.post(
+      "https://a2p.vodafone.ua/uaa/oauth/token",
+      null,
+      {
+        params: queryParams,
+        headers: headers,
+      }
+    );
+
+    if (token && data) {
+      const url =
+        "https://a2p.vodafone.ua/communication-event/api/communicationManagement/v2/communicationMessage/send";
+
+      const headers = {
+        "Content-Type": "application/json",
+        Accept: "*/*",
+        Authorization: `bearer ${token.data.access_token}`,
+      };
+      const data1 = {
+        content: `pin=${+data.smsInfo.machine_pin};setpin=${+data.pin};`,
+        type: "SMS",
+        receiver: [
+          { id: +data.smsInfo.id, phoneNumber: +data.smsInfo.machine_phone },
+        ],
+        sender: { id: "Vend Water" },
+        characteristic: [
+          { name: "DISTRIBUTION.ID", value: 5029972 },
+          { name: "VALIDITY.PERIOD", value: "000000000100000R" },
+        ],
+      };
+
+      const sendLitersData = await axios({
+        method: "post",
+        url: url,
+        headers: headers,
+        data: data1,
+      })
+        .then((response) => {
+          db.query(
+            `
+                     INSERT INTO sms_status (company_id,status,status_id,status_name)
+                     values (${+data.userData
+                       .company_id},'${"ACCEPTED"}',${8},'${"Заміна номера модуля"}')
+                     `,
+            (error, result) => {
+              if (error) {
+                console.error("Error executing query", error);
+              } else {
+                console.log("Query result:", result.rows);
+              }
+            }
+          );
+          res.status(200).json(response.data);
+          console.log(data);
+          db.query(
+            `update  water_machine set machine_phone = ${+data.machine_phone}
+             where machine_id =${+data.smsInfo.machine_id}
+            `
+          );
+
+          console.log("Response:", response.data);
+        })
+        .catch((error) => {
+          console.error("Error:", error.message);
+        });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      error: error,
+    });
+  }
+};
 
 module.exports = {
   sendMsgAddLitr,
@@ -491,4 +662,6 @@ module.exports = {
   sendPriceForLitr,
   sendGetInfo,
   changePin,
+  changeNumber,
+  changeToken
 };
--- routes/sendMessage.js
+++ routes/sendMessage.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const checkAuth = require("../middleware/checkAuth");
-const { sendMsgAddLitr, getToken, sendRestartModule, sendCollectCash, sendPriceForLitr, sendGetInfo, changePin } = require("../controllers/sendMessage");
+const { sendMsgAddLitr, getToken, sendRestartModule, sendCollectCash, sendPriceForLitr, sendGetInfo, changePin, changeNumber, changeToken } = require("../controllers/sendMessage");
 
 const router = express.Router();
 router.route('/').post(sendMsgAddLitr)
@@ -9,4 +9,6 @@ router.route('/collect-cash').post(sendCollectCash)
 router.route('/set-price').post(sendPriceForLitr)
 router.route('/get-info').post(sendGetInfo)
 router.route('/change-pin').post(changePin)
+router.route('/change-number').post(changeNumber)
+router.route('/change-token').post(changeToken)
 module.exports = router;
