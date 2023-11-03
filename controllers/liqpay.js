61a07516dac34a6a88c93754da19d9d794e9c3b8
tatarynrm <tatarynrm@gmail.com>
2023-11-03 15:21:53 +0200

    ewqewqewq

--- controllers/liqpay.js
+++ controllers/liqpay.js
@@ -1,4 +1,8 @@
 const LiqPay = require("../my_modules/liqpay");
+const liqpay = new LiqPay(
+  process.env.LIQPAY_PUBLIC_KEY,
+  process.env.LIQPAY_PRIVATE_KEY
+);
 const db = require("../db/db");
 const { v4: uuidv4 } = require("uuid");
 const iconv = require("iconv-lite"); // You may need to install the 'iconv-lite' package via npm
@@ -15,7 +19,6 @@ const encodingsToTry = [
   "iso-8859-1", // Latin-1
   "windows-1252", // Latin-1 (Windows)
 ];
-
 const stringEncodeFunc = (str) => {
   let decodedString = "";
   for (const encoding of encodingsToTry) {
@@ -28,23 +31,9 @@ const stringEncodeFunc = (str) => {
   }
   return decodedString;
 };
-// console.log(stringEncodeFunc(encodedString));
-
-const liqpay = new LiqPay(
-  process.env.LIQPAY_PUBLIC_KEY,
-  process.env.LIQPAY_PRIVATE_KEY
-);
 const createCheckout = async (req, res) => {
   const { amount, user_id, name, surname } = req.body;
-  console.log(req.body);
   try {
-    // const public_key = process.env.LIQPAY_PUBLIC_KEY;
-    // const private_key = process.env.LIQPAY_PRIVATE_KEY;
-
-    // const liqpay = new LiqPay(
-    //     process.env.LIQPAY_PUBLIC_KEY,
-    //     process.env.LIQPAY_PRIVATE_KEY
-    //   );
     const html = liqpay.cnb_form({
       action: "pay",
       amount: amount,
@@ -78,17 +67,12 @@ const createCheckout = async (req, res) => {
 const liqpayCallback = async (req, res) => {
   try {
     const data = req.body;
-    // Process the callback data as needed (e.g., update your records)
+
     const jsonData = atob(data.data); // Decode base64
     const jsonSignature = atob(data.signature); // Decode base64
     const orderData = JSON.parse(jsonData);
     const el = orderData;
-    // console.log("orderDATA", orderData);
-    // console.log("jsonSignature", jsonSignature);
     const orderId = orderData.order_id;
-    // console.log(orderData?.user_id);
-    // console.log("Order ID: ", orderId);
-    // console.log('elllllllllllllllllll',el);
     const decodeUsername = Buffer.from(
       el.sender_first_name,
       "utf-8"
@@ -99,21 +83,20 @@ const liqpayCallback = async (req, res) => {
     ).toString();
     let decodedName = stringEncodeFunc(el.sender_first_name);
     let decodedLastName = stringEncodeFunc(el.sender_last_name);
-    // const text = `INSERT INTO client_pay (payment_id, user_id,status,info,amount,sender_name,sender_surname,sender_card_mask2,sender_card_bank,date)
-    // VALUES ('${el.payment_id}','${el.user_id}','${el.status}','${el.info}','${el.amount}','${el.sender_name}','${el.sender_surname}','${el.sender_card_mask2}','${el.sender_card_bank}','${el.date}')
-    //  `
-
-    // Insert data with a parameterized query
-    //   const result =
-    //     await client.query(`INSERT INTO client_pay (payment_id, user_id,status,info,amount,sender_name,sender_surname,sender_card_mask2,sender_card_bank)
-    //     VALUES (${el.payment_id},${el.user_id},'${el.status}','${el.info}',${el.amount},'${el.sender_name}','${el.sender_surname}','${el.sender_card_mask2}','${el.sender_card_bank}')
-    //      `);
     const result =
       await db.query(`INSERT INTO client_pay (payment_id,amount,status,info,user_id,sender_name,sender_surname,sender_card_mask2,sender_card_bank)
         VALUES (${el.payment_id},${el.amount},'${el.status}','${el.info}',${el.customer},'${decodedName}','${decodedLastName}','${el.sender_card_mask2}','${el.sender_card_bank}')
          `);
 
     console.log("Data inserted successfully:", result);
+
+    liqpay.api("request", {
+        "action"   : "status",
+        "version"  : "3",
+        "order_id" : orderId
+        }, function( json ){
+        console.log( json.status );
+        });
   } catch (error) {
     console.log(error);
   }
