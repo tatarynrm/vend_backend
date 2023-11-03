6453012bac847e9dd65897563bf10982b37f58c6
tatarynrm <tatarynrm@gmail.com>
2023-11-03 14:40:49 +0200

    ewqewqeqw

--- controllers/liqpay.js
+++ controllers/liqpay.js
@@ -73,8 +73,8 @@ const liqpayCallback = async (req, res) => {
     //     VALUES (${el.payment_id},${el.user_id},'${el.status}','${el.info}',${el.amount},'${el.sender_name}','${el.sender_surname}','${el.sender_card_mask2}','${el.sender_card_bank}')
     //      `);
       const result =
-        await db.query(`INSERT INTO client_pay (payment_id,amount,status)
-        VALUES (${el.payment_id},${el.amount},'${el.status}')
+        await db.query(`INSERT INTO client_pay (payment_id,amount,status,info,user_id)
+        VALUES (${el.payment_id},${el.amount},'${el.status}','${el.info}',${el.user_id})
          `);
 
       console.log("Data inserted successfully:", result);
