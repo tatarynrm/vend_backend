36de31955bbffca80015d2b48c4cebbd6cc4b1d8
tatarynrm <tatarynrm@gmail.com>
2023-11-03 12:49:59 +0200

    ewqewq

--- controllers/liqpay.js
+++ controllers/liqpay.js
@@ -3,7 +3,7 @@ const db = require("../db/db");
 const { v4: uuidv4 } = require("uuid");
 
 const createCheckout = async (req, res) => {
-  const { amount ,user_id} = req.body;
+  const { amount, user_id } = req.body;
   console.log(user_id);
   try {
     const public_key = process.env.LIQPAY_PUBLIC_KEY;
@@ -36,27 +36,44 @@ const createCheckout = async (req, res) => {
       sender_first_name: "Roman",
       sender_last_name: "Tataryn",
       info: "VENDMARKET PAY FOR WATER MACHINE",
-      customer:+user_id
+      customer: +user_id,
     });
     res.json(html);
   } catch (error) {
     console.log(error);
   }
 };
-const liqpayCallback = (req,res) => {
+const liqpayCallback = async (req, res) => {
+  const client = await db.connect();
   try {
     const data = req.body;
     // Process the callback data as needed (e.g., update your records)
     const jsonData = atob(data.data); // Decode base64
     const jsonSignature = atob(data.signature); // Decode base64
     const orderData = JSON.parse(jsonData);
+    const el = orderData;
     console.log("orderDATA", orderData);
     console.log("jsonSignature", jsonSignature);
     const orderId = orderData.order_id;
     console.log(orderData?.user_id);
     console.log("Order ID: ", orderId);
+    if (orderData.status === "success") {
+      // const text = `INSERT INTO client_pay (payment_id, user_id,status,info,amount,sender_name,sender_surname,sender_card_mask2,sender_card_bank,date)
+      // VALUES ('${el.payment_id}','${el.user_id}','${el.status}','${el.info}','${el.amount}','${el.sender_name}','${el.sender_surname}','${el.sender_card_mask2}','${el.sender_card_bank}','${el.date}')
+      //  `
+
+      // Insert data with a parameterized query
+      const result =
+        await client.query(`INSERT INTO client_pay (payment_id, user_id,status,info,amount,sender_name,sender_surname,sender_card_mask2,sender_card_bank,date)
+        VALUES ('${el.payment_id}','${el.user_id}','${el.status}','${el.info}','${el.amount}','${el.sender_name}','${el.sender_surname}','${el.sender_card_mask2}','${el.sender_card_bank}','${el.date}')
+         `);
+
+      console.log("Data inserted successfully:", result);
+    }
   } catch (error) {
     console.log(error);
+  } finally {
+    client.release();
   }
 };
 module.exports = {
