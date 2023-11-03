0c8f6a545ca98e000576ea154b33fd8aa1392be1
tatarynrm <tatarynrm@gmail.com>
2023-11-03 10:59:36 +0200

    ewqewq

--- index.js
+++ index.js
@@ -60,34 +60,37 @@ const blockUserBeforePay = async () => {
 };
 blockUserBeforePay();
 
-const public_key = process.env.LIQPAY_PUBLIC_KEY;
-const private_key = process.env.LIQPAY_PRIVATE_KEY;
-const LiqPay = require("./my_modules/liqpay.js");
-const liqpay = new LiqPay(public_key, private_key);
-const html = liqpay.cnb_form({
-  action: "pay",
-  amount: "1",
-  currency: "UAH",
-  description: "Поповнення особистого кабінету vendmarket.space",
-  order_id: uuidv4(),
-  version: "3",
-  result_url: "https://vendmarket.space",
-  server_url: "https://api.vendmarket.space/callback",
-  rro_info: {
-    items: [
-      {
-        amount: 2,
-        price: 202,
-        cost: 404,
-        id: 123456,
-      },
-    ],
-    delivery_emails: ["tatarynrm@gmail.com", "rt@ict.lviv.ua"],
-  },
-  sender_first_name: "Roman",
-  sender_last_name: "Tataryn",
-});
-console.log(html);
+const getButtonToPay = ()=>{
+  const public_key = process.env.LIQPAY_PUBLIC_KEY;
+  const private_key = process.env.LIQPAY_PRIVATE_KEY;
+  const LiqPay = require("./my_modules/liqpay.js");
+  const liqpay = new LiqPay(public_key, private_key);
+  const html = liqpay.cnb_form({
+    action: "pay",
+    amount: "1",
+    currency: "UAH",
+    description: "Поповнення особистого кабінету vendmarket.space",
+    order_id: uuidv4(),
+    version: "3",
+    result_url: "https://vendmarket.space",
+    server_url: "https://api.vendmarket.space/callback",
+    rro_info: {
+      items: [
+        {
+          amount: 2,
+          price: 202,
+          cost: 404,
+          id: 123456,
+        },
+      ],
+      delivery_emails: ["tatarynrm@gmail.com", "rt@ict.lviv.ua"],
+    },
+    sender_first_name: "Roman",
+    sender_last_name: "Tataryn",
+  });
+  console.log(html);
+}
+getButtonToPay()
 // app.post('/makePayment', async (req, res) => {
 //   try {
 //     // Configure your Liqpay API credentials
@@ -137,6 +140,7 @@ app.post("/callback", (req, res) => {
   console.log("jsonSignature",jsonSignature);
   const orderId = orderData.order_id;
   console.log("Order ID: ", orderId);
+  console.log(orderData.description.toString("utf8"));
   liqpay.api(
     "request",
     {
