abc34225a861aa1cd50d1adcc76ba90557fbcdc5
tatarynrm <tatarynrm@gmail.com>
2024-04-18 14:14:26 +0300

    ewqewq

--- controllers/vendwaterController.js
+++ controllers/vendwaterController.js
@@ -13,7 +13,19 @@ const getAllMachinesPrice = async (req, res) => {
   }
 };
 
+const changeMachineValues = async (req,res)=>{
+    const { id,price,title } = req.body;
 
+  console.log(req.body);
+    try {
+      const result = await db.query('UPDATE vendwater_site SET machine_name = $1,machine_price = $2 WHERE id = $3', [title,price, id]);
+      res.status(200).json({ message: 'Record updated successfully' });
+    } catch (error) {
+      console.error('Error updating record:', error);
+      res.status(500).json({ error: 'An error occurred while updating record' });
+    }
+}
 module.exports = {
-    getAllMachinesPrice
+    getAllMachinesPrice,
+    changeMachineValues
 };
--- routes/vendwaterController.js
+++ routes/vendwaterController.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const checkAuth = require("../middleware/checkAuth");
 const { getAllUsers, createNewUser, userUpdate, userDelete, cancelActiveFalse } = require("../controllers/user");
-const { getAllMachinesPrice } = require("../controllers/vendwaterController");
+const { getAllMachinesPrice, changeMachineValues } = require("../controllers/vendwaterController");
 
 const router = express.Router();
 
 router.route('/').get(getAllMachinesPrice)
+router.route('/update-machine').post(changeMachineValues)
 
 
 module.exports = router;
