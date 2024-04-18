30d5ba08544cdd4c4479ae50cabd467918b44b15
tatarynrm <tatarynrm@gmail.com>
2024-04-18 16:50:25 +0300

    ewqewqeqw

--- controllers/vendwaterController.js
+++ controllers/vendwaterController.js
@@ -12,11 +12,21 @@ const getAllMachinesPrice = async (req, res) => {
     console.log(error);
   }
 };
+const getSiteContacts = async (req, res) => {
+  try {
+    const result = await db.query(
+      `select * from vendwater_contacts`
+    );
 
+    res.status(200).json(result.rows[0]);
+  } catch (error) {
+    console.log(error);
+  }
+};
 const changeMachineValues = async (req,res)=>{
     const { id,price,title } = req.body;
 
-  console.log(req.body);
+
     try {
       const result = await db.query('UPDATE vendwater_site SET machine_name = $1,machine_price = $2 WHERE id = $3', [title,price, id]);
       res.status(200).json({ message: 'Record updated successfully' });
@@ -25,7 +35,27 @@ const changeMachineValues = async (req,res)=>{
       res.status(500).json({ error: 'An error occurred while updating record' });
     }
 }
+const changeNumber = async (req,res)=>{
+    const {phone,id} = req.body;
+console.log(req.body);
+
+    try {
+      const result = await db.query('UPDATE vendwater_contacts SET phone = $1 WHERE id = $2', [phone,id]);
+      console.log(result);
+      res.status(200).json({ message: 'Record updated successfully' });
+    } catch (error) {
+      console.error('Error updating record:', error);
+      res.status(500).json({ error: 'An error occurred while updating record' });
+    }
+}
+
+
+// Contacts
+
+
 module.exports = {
     getAllMachinesPrice,
-    changeMachineValues
+    changeMachineValues,
+    getSiteContacts,
+    changeNumber
 };
--- routes/vendwaterController.js
+++ routes/vendwaterController.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const checkAuth = require("../middleware/checkAuth");
 const { getAllUsers, createNewUser, userUpdate, userDelete, cancelActiveFalse } = require("../controllers/user");
-const { getAllMachinesPrice, changeMachineValues } = require("../controllers/vendwaterController");
+const { getAllMachinesPrice, changeMachineValues, getSiteContacts, changeNumber } = require("../controllers/vendwaterController");
 
 const router = express.Router();
 
 router.route('/').get(getAllMachinesPrice)
 router.route('/update-machine').post(changeMachineValues)
+router.route('/update-number').post(changeNumber)
+router.route('/contacts').get(getSiteContacts)
 
 
 module.exports = router;
